452b405d84fc652242f77943a12cdd4eb564d667
soumaya <ghoulsoumaya95@gmail.com>
2018-07-25 19:55:18 +0100

    Init commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path=require('path');
+const express=require('express');
+const publicPath =path.join(__dirname,'../public');
+
+var app = express();
+var app = express();
+const port = process.env.PORT;
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
