93158a56eadf075cd6afe8bcfcb7f74e755bdc9b
soumaya <ghoulsoumaya95@gmail.com>
2018-07-26 19:25:19 +0100

    emit newMessage en createMessage

--- server/server.js
+++ server/server.js
@@ -17,10 +17,11 @@ console.log('new user connected');
 
 socket.emit('newMessage',generateMessage('admin','welcome to the chat app'));
 socket.broadcast.emit('newMessage',generateMessage('admin','welcome to the chat app'));
-socket.on('createMessage',(newMessage)=>{
+socket.on('createMessage',(newMessage,callback)=>{
 console.log('create email',newMessage);
-io.emit('newMessage',generateMessage(message.from,message.text));
-/*socket.broadcast.emit('newMessage',{
+io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
+
+callback('This is from the server');/*socket.broadcast.emit('newMessage',{
 	from :newMessage.from,
 	text:newMessage.text,
 	createAt:new Date().getTime()
