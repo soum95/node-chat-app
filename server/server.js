f540aaa30458002872b598432a0857d667e8dd71
soumaya <ghoulsoumaya95@gmail.com>
2018-07-26 19:04:51 +0100

    emit newMessage en createMessage

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express=require('express');
 const publicPath =path.join(__dirname,'../public');
 const socketIO=require('socket.io');
 const http=require('http');
+const {generateMessage}=require('./utiles/message');
+
 var app = express();
 
 
@@ -13,22 +15,11 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 console.log('new user connected');
 
-socket.emit('newMessage',{
-	from :'admin',
-	text:'welcome to the chat app',
-	createAt:new Date().getTime()
-});
-socket.broadcast.emit('newMessage',{
-	from :'admin',
-	text:'new userjoined',
-	createAt:new Date().getTime()});
+socket.emit('newMessage',generateMessage('admin','welcome to the chat app'));
+socket.broadcast.emit('newMessage',generateMessage('admin','welcome to the chat app'));
 socket.on('createMessage',(newMessage)=>{
 console.log('create email',newMessage);
-io.emit('newMessage',{
-	from :newMessage.from,
-	text:newMessage.text,
-	createAt:new Date().getTime()
-});
+io.emit('newMessage',generateMessage(message.from,message.text));
 /*socket.broadcast.emit('newMessage',{
 	from :newMessage.from,
 	text:newMessage.text,
