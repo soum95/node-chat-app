3f2bbcabbfda029d761f1808f1b31ad08c6b8281
soumaya <ghoulsoumaya95@gmail.com>
2018-07-26 18:29:02 +0100

    emit newMessage en createMessage

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ var io=socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 console.log('new user connected');
+
+socket.emit('newMessage',{
+	from :'admin',
+	text:'welcome to the chat app',
+	createAt:new Date().getTime()
+});
+socket.broadcast.emit('newMessage',{
+	from :'admin',
+	text:'new userjoined',
+	createAt:new Date().getTime()});
 socket.on('createMessage',(newMessage)=>{
 console.log('create email',newMessage);
 io.emit('newMessage',{
@@ -19,6 +29,11 @@ io.emit('newMessage',{
 	text:newMessage.text,
 	createAt:new Date().getTime()
 });
+/*socket.broadcast.emit('newMessage',{
+	from :newMessage.from,
+	text:newMessage.text,
+	createAt:new Date().getTime()
+});*/
 });
 socket.on('disconnect',()=>{
 console.log('user desconnected');
